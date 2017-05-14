71f576ce52afd6fe5ef70e6ca54b9b0556e1fd72
JuanjoVG <juan.jose.vazquez.gimenez@est.fib.upc.edu>
2017-05-14 05:25:47 +0200

    Add init_db to insert the datasets

--- backend/init_db.py
+++ backend/init_db.py
@@ -9,21 +9,51 @@ db = MySQLdb.connect(host="localhost",    # your host, usually localhost
 # app.config['MYSQL_DB'] = 'SaveSound'
 # app.config['MYSQL_HOST'] = 'localhost'
 # app.config['MYSQL_PASSWORD'] = ''
+#
+# query = ''
+# f = open("contratos.csv", 'rb')
+# for ind, row in enumerate(f):
+#     if ind is 0: continue
+#     str_split = str(row).split(',')
+#     municipio = str_split[0].split('"')[1]
+#     distrito = str_split[1].split('"')[1]
+#     id_contrato = str_split[4].split('"')[1]
+#     query += "('" + id_contrato + "', '" + municipio + "', '" + distrito + "')"
+#
+#     if ind%100 is 0:
+#         print(ind)
+#         cur = db.cursor()
+#         q = "INSERT INTO contratos(id, municipio, distrito) VALUES" + query + ';'
+#         #print(q)
+#         cur.execute(q)
+#         rv = cur.fetchall()
+#         query = ''
+#     else:
+#         query += ','
+#
+# cur = db.cursor()
+# q = "INSERT INTO contratos(id, municipio, distrito) VALUES" + query[:-1] + ';'
+# print(q)
+# cur.execute(q)
+# rv = cur.fetchall()
+
+
+####
+
 
 query = ''
-f = open("contratos.csv", 'rb')
+f = open("consumos.csv", 'rb')
 for ind, row in enumerate(f):
     if ind is 0: continue
     str_split = str(row).split(',')
-    municipio = str_split[0].split('"')[1]
-    distrito = str_split[1].split('"')[1]
-    id_contrato = str_split[4].split('"')[1]
-    query += "('" + id_contrato + "', '" + municipio + "', '" + distrito + "')"
+    fecha = str_split[0].split("'")[1]
+    consumo = str_split[1]
+    contrato_id = str_split[2].split('"')[1]
+    query += "('" + fecha + "', " + consumo + ", '" + contrato_id + "')"
 
     if ind%100 is 0:
-        print(ind)
         cur = db.cursor()
-        q = "INSERT INTO contratos(id, municipio, distrito) VALUES" + query + ';'
+        q = "INSERT INTO consumos(fecha, consumo, contrato_id) VALUES" + query + ';'
         #print(q)
         cur.execute(q)
         rv = cur.fetchall()
@@ -32,8 +62,12 @@ for ind, row in enumerate(f):
         query += ','
 
 cur = db.cursor()
-q = "INSERT INTO contratos(id, municipio, distrito) VALUES" + query[:-1] + ';'
-print(q)
+q = "INSERT INTO consumos(fecha, consumo, contrato_id) VALUES" + query[:-1] + ';'
 cur.execute(q)
 rv = cur.fetchall()
+
+
+
+
+
 db.close()
