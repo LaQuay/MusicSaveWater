a149d3d7a51985fd41f4d5428b7785105a557347
Juan Salmerón Moya <jnssterrass@gmail.com>
2017-05-14 09:36:50 +0300

    all first iteration routes

--- backend/run_flask_api.py
+++ backend/run_flask_api.py
@@ -17,28 +17,44 @@ def tracks(type, token):
     return jsonify(sp.current_user_top_tracks())
 
 
-@app.route("/user/<user_id>/achievements", methods=['PUT'])
-def put_user_achievement(user_id):
+@app.route("/user/<user_id>/showers", methods=['PUT'])
+def put_user_shower(user_id):
+    #user + type + duration + date
     return
 
 
-@app.route("/contract/<contract_id>/achievements", methods=['PUT'])
-def put_contract_achievement(contract_id):
-    return
+#@app.route("/user/<user_id>/achievements", methods=['PUT'])
+#def put_user_achievement(user_id):
+#    return
 
 
 @app.route("/user/<user_id>/achievements", methods=['GET'])
 def get_user_achievements(user_id):
+    #description + target + progress
     return
 
 
 @app.route("/contract/<contract_id>/achievements", methods=['GET'])
 def get_contract_achievements(contract_id):
+    # description + target + progress
     return
 
 
-@app.route("/user/<user_id>/showers", methods=['PUT'])
-def put_user_shower(user_id):
+@ app.route("/user/<user_id>/showers", methods=['GET'])
+def get_user_showers(user_id):
+    #type + duration + date
+    return
+
+
+@ app.route("/contract/<contract_id>/showers", methods=['GET'])
+def get_contract_showers(contract_id):
+    #type + duration + date + user
+    return
+
+
+@ app.route("/contract/<contract_id>/invoices", methods=['GET'])
+def get_contract_invoices(contract_id):
+    #date + consumption
     return
 
 
--- backend/spotify_module.py
+++ backend/spotify_module.py
@@ -6,9 +6,13 @@ class Spotishower:
         self.scope = '-'
         self.username = "ggrimm"
         self.token = "BQDHFVbGlutWEzf3rfI3xm4V_OGTSKOG7IxWz4Um9CE7ZiDJLUfFEPgPvLAHkjZbkBbS8U6tgT-hPogD9gXIhnp9XMCw1eyRlMMJGcEu5reKrggULEFMak0xSnGvVWBzOoyVFO2zVVbxPAO8W431XnYUvTT4Zaepv7KOq_Hbbf5-ksNdxXKkddWdhrJWxaHS8dP7_7ers07cRQRjBxFEGh25yLx1WW17Z9ofMjq7AO-hDXYpbPnNuAPlHoqvV8P2tsMxcBqtGH3rVCTho60tCtHqxgI-C6BTUuDoVLZQe-agK3kckRE5wLCO2LgYyw"
+        self.limit = 10
 
     def set_user_token(self, token):
         self.token = token
+
+    def set_limit(self,limit):
+        self.limit = limit
 
     def current_user_top_tracks(self):
         if self.token:
@@ -17,7 +21,7 @@ class Spotishower:
             # ranges = ['short_term', 'medium_term', 'long_term']
             ranges = ['medium_term']
             for range in ranges:
-                results = sp.current_user_top_tracks(time_range=range, limit=5)
+                results = sp.current_user_top_tracks(time_range=range, limit=10)
                 print(results['items'])
                 return results['items']
 
