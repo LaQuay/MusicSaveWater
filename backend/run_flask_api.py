86a55b741e75fde18b0bde215218aa2a725c1aee
Juan Salmerón Moya <jnssterrass@gmail.com>
2017-05-14 14:06:54 +0300

    fix sql query finally ¬¬

--- backend/run_flask_api.py
+++ backend/run_flask_api.py
@@ -86,7 +86,8 @@ def tracks(type, token):
 @app.route("/user/<user_id_param>", methods=['PUT'])
 def put_user(user_id_param):
     cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO users (user_id) VALUES ("+user_id_param+");")
+    #cur.execute("INSERT INTO users (id) VALUES (MIAUUSER);")
+    cur.execute("INSERT INTO users (id) VALUES ("+user_id_param+");")
     return
 
 
