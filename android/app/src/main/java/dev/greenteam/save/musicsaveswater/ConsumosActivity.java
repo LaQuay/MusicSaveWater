135b4c2bc63d63b0161265580e754c3138e58c21
Marc Vila <marcvilagomez@gmail.com>
2017-05-14 14:47:47 +0200

    Panel de consumos

--- android/app/src/main/java/dev/greenteam/save/musicsaveswater/ConsumosActivity.java
+++ android/app/src/main/java/dev/greenteam/save/musicsaveswater/ConsumosActivity.java
@@ -1,41 +1,39 @@
 package dev.greenteam.save.musicsaveswater;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
 
 /**
  * Created by LaQuay on 14/05/2017.
  */
 
-public class ShowerActivity extends AppCompatActivity {
-    public static final String TAG = ShowerActivity.class.getSimpleName();
+public class ConsumosActivity extends AppCompatActivity {
+    public static final String TAG = ConsumosActivity.class.getSimpleName();
     private ImageView buttonTerminar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shower);
+        setContentView(R.layout.activity_consumos);
 
         setUpElements();
         setUpListeners();
     }
 
     private void setUpElements() {
-        buttonTerminar = (ImageView) findViewById(R.id.activity_login_terminado_image);
+        //buttonTerminar = (ImageView) findViewById(R.id.activity_consumo_terminado_image);
     }
 
     private void setUpListeners() {
-        buttonTerminar.setOnClickListener(new View.OnClickListener() {
+        /*buttonTerminar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(ShowerActivity.this, ConsumosActivity.class);
+                Intent myIntent = new Intent(ConsumosActivity.this, MainActivity.class);
                 myIntent.putExtra("key", ""); //Optional parameters
                 startActivity(myIntent);
             }
-        });
+        });*/
     }
 }
 
